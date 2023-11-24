c9a43374146ebce3e3eb9255ed38095868026e44
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2023-11-24 17:28:24 +0100

    Jour 2

--- apiPublic/script.js
+++ apiPublic/script.js
@@ -1,4 +1,4 @@
-const weatherAPIKey = '39b22adfc8928a19036080e1688e4e3cb9d6424b96eced73430d78933fbb7fa6' //Clé API
+/*const weatherAPIKey = '39b22adfc8928a19036080e1688e4e3cb9d6424b96eced73430d78933fbb7fa6' //Clé API
 
 const data = [
     {name : 'Sandrine'}, 
@@ -85,21 +85,74 @@ const cardMap = data.map(e => {
             <div class="card">
                 <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${Object.keys(e)[0]} ${e.name}</h5>
+                    <h5 class="card-title">${Object.keys(e)[0]} ${e[Object.keys(e)[0]]}</h5>
                 </div>
             </div>
         </div>
         `
         return htmlElement
-        
-}
-)
+})
 
 const container = document.getElementById("apiContainer")
-container.append(...cardMap)
+container.append(...cardMap)*/
 /*
 container.append(cardMap[0])
 container.append(cardMap[1])
 container.append(cardMap[2])
 */
 //....
+
+// PARTIE API POKEMON
+
+const container = document.getElementById("apiContainer")
+
+container.innerHTML = '<div class="container" id="apiContainer"></div>'
+
+const templateCard = (sprites, name, weight, url) => (`
+<div class="col-md-4">
+    <div class="card">
+        <img src="${sprites.front_default}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">Son poids est de : ${weight} Kg</p>
+            <a href="${url}" class="btn btn-primary">Voir sa fiche</a>
+        </div>
+    </div>
+</div>
+`)
+
+fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=151')
+.then(response => response.json())
+.then(res => {
+    const componentsPokemon = res.results.map( ({url}) => {
+        const divCard = document.createElement('div')
+        fetch(url)
+            .then(response => response.json())
+            .then(({sprites, name, weight}) => divCard.innerHTML = templateCard(sprites, name, weight, url))
+        .catch(err => {
+            console.error(err)
+            return
+        })
+        return divCard
+    })
+    container.append(...componentsPokemon)
+})
+.catch(err => console.error(err))
+
+const validBTN = document.getElementById('validBTN'), 
+passInput = document.getElementById('passInput'),
+mailInput = document.getElementById('mailInput'),
+formContainer = document.getElementById('formContainer')
+
+validBTN.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(mailInput.value.length < 5 || passInput.value.length < 8){
+        console.error('BAD CREDENTIALS')
+        const alertMessage = document.createElement('div')
+        alertMessage.innerHTML = '<div class="alert alert-danger" role="alert"><p>Problème d\'identifiant</p></div>'
+        formContainer.append(alertMessage)
+    }
+    else {
+        console.log('EVERYTHING\'S FINE')
+    }
+})
