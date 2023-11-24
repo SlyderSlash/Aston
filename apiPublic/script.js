35043bde27e974e34a213b7efa5235e0af985dd7
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2023-11-24 14:23:58 +0100

    map test

--- apiPublic/script.js
+++ apiPublic/script.js
@@ -1,5 +1,13 @@
 const weatherAPIKey = '39b22adfc8928a19036080e1688e4e3cb9d6424b96eced73430d78933fbb7fa6' //Clé API
 
+const data = [
+    {name : 'Sandrine'}, 
+    {firstname : 'Teddy'},
+    {lastname : 'Remy'}, 
+    {name : 'Pape'}, 
+    {name : 'Olivier'}
+]
+
 const getLocation = () => {
   // Récupération de la longitude et latitude
   console.log(1)
@@ -31,22 +39,22 @@ const getWeather = (lon, lat) => {
         res.json()
             .then(res => {
                 console.log(res)
-                //On récupère l'élément container
+                // On récupère l'élément container
                 const container = document.getElementById("apiContainer")
                 // on créer un élément
                 const divCard = document.createElement('div')
                 // On intègre le code de la card
                 divCard.innerHTML = `
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${res.city.name}</h5>
-                            <p class="card-text">Le soleil se lève à ${res.ephemeride.sunrise}</p>
-                            <a href="#" class="btn btn-primary">${res.city.cp}</a>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${res.city.name}</h5>
+                                <p class="card-text">Le soleil se lève à ${res.ephemeride.sunrise}</p>
+                                <a href="#" class="btn btn-primary">${res.city.cp}</a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 `
                 container.append(divCard)
             })
@@ -69,3 +77,29 @@ const getWeather = (lon, lat) => {
   // Conversion en JSON
   // Extraction des données voulues
 }
+
+const cardMap = data.map(e => {
+        const htmlElement = document.createElement('div')
+        htmlElement.innerHTML = `
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${Object.keys(e)[0]} ${e.name}</h5>
+                </div>
+            </div>
+        </div>
+        `
+        return htmlElement
+        
+}
+)
+
+const container = document.getElementById("apiContainer")
+container.append(...cardMap)
+/*
+container.append(cardMap[0])
+container.append(cardMap[1])
+container.append(cardMap[2])
+*/
+//....
